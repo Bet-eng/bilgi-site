bb436a5fc632c7f29df8081b7f8fd79f9eb19341
Bet-eng <betul4086@gmail.com>
2026-02-12 18:49:30 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Merhaba Dünya"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(host="0.0.0.0", port=10000)
