47cf69db350ecbc75fb4a985b5c121ebf5f793ea
Bet-eng <betul4086@gmail.com>
2026-02-14 23:23:02 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "SELAAMM KANKAAA LOVE YOUUU <3"
-
-app.run(host="0.0.0.0", port=10000)
 from flask import Flask, render_template
 
 app = Flask(__name__)
